7c8ed40b5492c7c7ef97e80c41c7eefa78685ecb
Frank J. Reashore <frank.reashore@gov.bc.ca>
2017-11-06 11:27:37 -0800

    Menu App working

--- src/App.js
+++ src/App.js
@@ -1,17 +1,6 @@
 
 import React, { Component } from 'react';
-import './App.css';
+//import './App.css';
 
-
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Menu App</h1>
-      </div>
-    );
-  }
-}
 
 export default App;
